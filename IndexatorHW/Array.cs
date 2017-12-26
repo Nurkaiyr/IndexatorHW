7a4192f8ab6e7a0676d0cf37fb3ada97e228830d
Nurkaiyr <yedige21@gmail.com>
2017-12-27 00:22:55 +0600

    hw

---
+++ IndexatorHW/Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorHW
+{
+    public class Array
+    {
+        private int[] array;
+        public int Size { get; set; }
+
+        public Array()
+        {
+            Size = 1;
+            array = new int[Size];
+        }
+
+        public Array(int size)
+        {
+            Size = size;
+            array = new int[Size];
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                return array[i];
+            }
+            set
+            {
+                array[i] = (int)Math.Pow((double)value, 2);
+            }
+        }
+    }
+}
---
+++ IndexatorHW/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexatorHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array(3);
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.WriteLine(array[i]+" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
